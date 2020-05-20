b2e992840cc56c727c3809e5344e1cd70867ce73
Ang3l1t0 <1546@holbertonschool.com>
2020-05-20 12:17:11 -0500

    pep8 corrections

--- Cars_inheritance.py
+++ Cars_inheritance.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-
 class Carro():
     def __init__(self, cars, actions):
         self.cars = cars
         self.actions = actions
+
     def __str__(self):
         return "Cars: {}, Action: {},".format(self.cars, self.actions)
+
 
 class Cars(Carro):
     def __init__(self, cars, actions, brand, year, color):
@@ -13,8 +14,11 @@ class Cars(Carro):
         self.brand = brand
         self.year = year
         self.color = color
+
     def __str__(self):
-        return Carro.__str__(self) + " Brand: {}, Year: {}, Color: {}".format(self.brand, self.year, self.color)
+        return (Carro.__str__(self) +
+                "Brand: {}, Year: {}, Color: {}"
+                .format(self.brand, self.year, self.color))
 
 Car1 = Cars("vehicle", True, "Ford", "2019", "Red")
 Car2 = Cars("vehicle", False, "Toyota", "1955", "GreenViche")
