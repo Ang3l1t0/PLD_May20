972e12c0dfd940b09ec608d2aaebc51d003c4453
AdrianDeLaAsuncionBuelvas <delaasuncionbuelvasadrian@gmail.com>
2020-05-20 12:21:09 -0500

    add Correction

---
+++ Cars_inheritance.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Carro():
+    def __init__(self, cars, actions):
+        self.cars = cars
+        self.actions = actions
+    def __str__(self):
+        return "Cars: {}, Action: {},".format(self.cars, self.actions)
+
+class Cars(Carro):
+    def __init__(self, cars, actions, brand, year, color):
+        Carro.__init__(self, cars, actions)
+        self.brand = brand
+        self.year = year
+        self.color = color
+    def __str__(self):
+        show = " Brand:{}, Year:{}, Color:{}"
+        return Carro.__str__(self)+ show.format(self.brand, self.year, self.color)
+
+Car1 = Cars("vehicle", True, "Ford", "2019", "Red")
+Car2 = Cars("vehicle", False, "Toyota", "1955", "GreenViche")
+print(Car1)
+print(Car2)
